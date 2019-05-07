80900924a1f8aca952126138abfebcb011620b53
zengge <gzeng@thoughtworks.com>
2019-05-07 10:22:30 +0800

    targets

--- packages/eslint/index.js
+++ packages/eslint/index.js
@@ -9,4 +9,4 @@ export function loadPerson () {
   return Promise.resolve('ok')
 }
 
-console.log(window.name) // eslint-disable-line no-console, no-undef
+console.log(window.name + ""); // eslint-disable-line no-console, no-undef, semi, quotes
